1769db8d5f5fee654efe128e6fd4aa4de34c726a
David <david@ozoono.com>
2019-10-09 01:15:16 +0200

    link repo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer">
-    <a href="https://github.com/ozoono/vue-table">Repo on Github</a>
+    <a href="https://github.com/ozoono/vue-table-component">Repo on Github</a>
     |
     Created with <a href="http://vuejs.org">Vue 2.x</a>   
   </div> 
